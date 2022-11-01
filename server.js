402264fec0a9a5abeb2b5eb7edcf6b758f15ee3a
Mariana West Hartenthal <97693233+marianaWest@users.noreply.github.com>
2022-11-01 09:37:04 +0000

    add link to google fonts

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 //const postRoutes = require("./routes/posts");
 
-
-
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
 
