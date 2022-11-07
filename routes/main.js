746f65350ff98851d523a273a8df898a945a69d6
borojetski <borojetski@gmail.com>
2022-11-06 20:58:55 -1000

    Comment main.js file.

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+//establish home controller that leads to login/signup page
 const homeController = require("../controllers/home");
 
 router.get("/", homeController.getIndex);
