c41bbce5f89cde878eea50537ea1bb9d5085890f
borojetski <borojetski@gmail.com>
2022-11-08 22:40:46 -1000

    Add get and post routes for login and signup auth controllers.

--- routes/main.js
+++ routes/main.js
@@ -5,5 +5,9 @@ const router = express.Router();
 const homeController = require("../controllers/home");
 
 router.get("/", homeController.getIndex);
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
 
 module.exports = router;
