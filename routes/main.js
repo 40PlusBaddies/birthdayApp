2e166bfbdaaa4fd706039f39399f928152f5959c
borojetski <borojetski@gmail.com>
2022-11-10 20:16:40 -1000

    Add profile and logout routes to main.js.

--- routes/main.js
+++ routes/main.js
@@ -7,8 +7,10 @@ const authController = require("../controllers/auth");
 
 //main routes
 router.get("/", homeController.getIndex);
+router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
+router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
 
