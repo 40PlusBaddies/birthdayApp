8132b58afd4271394a35b31f9936c9771f83cce5
borojetski <borojetski@gmail.com>
2022-12-02 08:14:22 -1000

    Add createPost route to main and modify /createPost dir.

--- routes/posts.js
+++ routes/posts.js
@@ -7,7 +7,7 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Post Routes - simplified for now
 router.get("/:id", ensureAuth, postsController.getPost);
 
-router.post("/createPost", postsController.createPost);
+// router.post("/createPost", postsController.createPost);
 
 router.put("/likePost/:id", postsController.likePost);
 
