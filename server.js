7c4ccb3d865861d7ac75db010064423a8e4b8f0e
Chandra Schreck <chandrarose2003@yahoo.com>
2022-12-04 09:03:45 -0700

    fixed post/createPost routing

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
-app.use("/posts", postRoutes);
+app.use("/post", postRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
