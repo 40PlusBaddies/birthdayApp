c307ea5061a81e9f3666b9e884f9c50e56acb311
borojetski <borojetski@gmail.com>
2022-12-06 23:20:13 -1000

    Comment out gifts object.

--- models/BirthdayPerson.js
+++ models/BirthdayPerson.js
@@ -17,7 +17,7 @@ const BirthdayPersonSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
-  gifts: [String]*/
+  //gifts: [String]
 })
 
 module.exports = mongoose.model('BirthdayPerson', BirthdayPersonSchema)
